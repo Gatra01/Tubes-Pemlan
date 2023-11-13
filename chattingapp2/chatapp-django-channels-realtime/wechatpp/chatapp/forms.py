41cdc96da0479f042fddae539134931bf01fe356
Gatra01 <87842559+Gatra01@users.noreply.github.com>
2023-11-13 18:15:23 +0700

    Delete chattingapp2/chatapp-django-channels-realtime directory

--- chattingapp2/chatapp-django-channels-realtime/wechatpp/chatapp/forms.py
+++
@@ -1,6 +0,0 @@
-# Buat file forms.py di direktori aplikasi Anda
-
-from django import forms
-
-class PersonalChatForm(forms.Form):
-    message = forms.CharField(widget=forms.Textarea(attrs={'rows': 2}))
